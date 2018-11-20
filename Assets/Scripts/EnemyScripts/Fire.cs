1344267befd297255486a8ea1edfe823bb3aec7d
Jordey <jordeysierhuis@hotmail.nl>
2018-11-20 16:50:20 +0100

    fire almost

--- Assets/Scripts/EnemyScripts/Fire.cs
+++ Assets/Scripts/EnemyScripts/Fire.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class Fire : MonoBehaviour {
 
+    int p;
+    public GameObject bullet;
+
 	// Use this for initialization
 	void Start () {
-		
+        p = 60;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+
+        p--;
+        if(p < 1)
+        {
+            p = 60;
+            shoot();
+        }
+
+
 	}
+
+    void shoot()
+    {
+
+    }
+
 }
