5888b8208f74a269b0b5d0ae2a48e7f83944a552
Jordey <jordeysierhuis@hotmail.nl>
2018-11-20 13:19:42 +0100

    deathblock done

---
+++ Assets/Scripts/BlockScripts/MoveKillBlock.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveKillBlock : MonoBehaviour
+{
+
+    public Vector2 movePath;
+    Vector2 moveHold;
+
+    bool activate;
+
+    // Use this for initialization
+    void Start()
+    {
+        activate = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        if (activate == true)
+        {
+            move();
+        }
+    }
+
+
+    public void move()
+    {
+        moveHold = new Vector2(transform.position.x + movePath.x, transform.position.y + movePath.y);
+        transform.position = Vector2.Lerp(transform.position, moveHold, 0.5f);
+    }
+
+    public void triggered()
+    {
+        activate = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //kill player
+            //collision.gameObject.GetComponent<>();
+        }
+    }
+
+}
