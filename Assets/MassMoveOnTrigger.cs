1386f4a6247de636730f1ba009921851628cffb8
Luc Schuttel <luc_schuttel@hotmail.com>
2018-11-21 14:51:41 +0100

    zijn lekker

---
+++ Assets/MassMoveOnTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MassMoveOnTrigger : MonoBehaviour {
+
+    GameObject player;
+    Transform[] items;
+    Transform mTrigger;
+    bool picked;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.Find("Player");
+        picked = false;
+
+        mTrigger = GameObject.Find("MassTrigger").transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (player != null)
+        {
+            if (Vector3.Distance(player.transform.position, transform.position) < 1.2f)
+            {
+                picked = true;
+            }
+        }
+
+        if (picked)
+        {
+            mTrigger.Translate(new Vector2(0, 0.1f));
+        }
+	}
+}
---
+++ Assets/NukeBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NukeBehaviour : MonoBehaviour {
+
+    Vector3 startPoint;
+
+	// Use this for initialization
+	void Start () {
+        startPoint = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y > 18)
+        {
+            transform.position = startPoint;
+        }
+	}
+}
