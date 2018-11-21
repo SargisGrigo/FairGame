5ba01938ddf145436f526d1ec378ea9322afd2c1
Luc Schuttel <luc_schuttel@hotmail.com>
2018-11-21 10:31:17 +0100

    Added Jordey's Prefabs + Scripts

---
+++ Assets/Scripts/EnemyScripts/Chase.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chase : MonoBehaviour
+{
+
+    public int speed;
+    float speedHold;
+
+    float moveX, moveY;
+
+    Vector2 move;
+    Vector2 moveHold;
+
+    GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        speedHold = speed;
+        speedHold /= 100;
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (player != null)
+        {
+
+
+            if (player.transform.position.x < transform.position.x)
+            {
+                moveX = -speedHold;
+            }
+            else if (player.transform.position.x > transform.position.x)
+            {
+                moveX = speedHold;
+            }
+            else
+            {
+                moveX = 0;
+            }
+
+            if (player.transform.position.y < transform.position.y)
+            {
+                moveY = -speedHold;
+            }
+            else if (player.transform.position.y > transform.position.y)
+            {
+                moveY = speedHold;
+            }
+            else
+            {
+                moveY = 0;
+            }
+
+            moveHold = new Vector2(transform.position.x + move.x, transform.position.y + move.y);
+            transform.position = Vector2.Lerp(transform.position, moveHold, 1);
+
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyScripts/Fire.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+
+    int p;
+    public int frequency;
+    public GameObject bullet;
+
+    // Use this for initialization
+    void Start()
+    {
+        p = 60 * frequency;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        p--;
+        if (p < 1)
+        {
+            p = 60 * frequency;
+            shoot();
+        }
+
+
+    }
+
+    void shoot()
+    {
+        GameObject shot;
+        shot = Instantiate(bullet, transform.position, transform.rotation, transform);
+    }
+
+}
